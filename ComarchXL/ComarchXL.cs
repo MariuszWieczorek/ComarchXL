838d28dc2a3e2a502af75af6ae6bc3f7a2f4965f
Mariusz Wieczorek <mariusz.wieczorek@Kabat.local>
2020-06-09 08:20:46 +0200

    Add project files.

---
+++ ComarchXL/ComarchXL.cs
@@ -0,0 +1,40 @@
+﻿using cdn_api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ComarchXL
+{
+    public static class ComarchTools
+    {
+        public static int  zaloguj(ref int SessionId)
+        {
+            var Login = new XLLoginInfo_20201
+            {
+                Wersja = 20201,
+                ProgramID = "test",
+                OpeIdent = "admin",
+                OpeHaslo = "1111",
+                Baza = "KABAT_TEST"
+
+            };
+            
+            cdn_api.cdn_api.XLLogin(Login, ref SessionId);
+            MessageBox.Show(SessionId.ToString());
+            
+            return SessionId;
+        }
+
+        public static int wyloguj(int SessionId)
+        {
+            int retValue = cdn_api.cdn_api.XLLogout(SessionId);
+            MessageBox.Show(retValue.ToString());
+            return retValue;
+        }
+
+
+    }
+}
---
+++ ComarchXL/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using cdn_api;
+
+namespace ComarchXL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        Int32 SessionID = -1;
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            ComarchTools.zaloguj(ref SessionID);
+          
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ComarchTools.wyloguj(SessionID);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Int32 TowarID = -1;
+            var Towar = new XLTowarInfo_20201
+            {
+                Wersja = 20201,
+                Typ = 2,
+                Kod = "PPPZZZ",
+                Nazwa = "PPPZZZ",
+                Jm  = "szt"
+            };
+
+            
+            var res = cdn_api.cdn_api.XLNowyTowar(SessionID,ref TowarID, Towar);
+            MessageBox.Show(res.ToString());
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Int32 IDReceptury = -1;
+            var RecepturaInfo = new XLRecepturaInfo_20201
+            {
+                Wersja = 20201,
+                TypReceptury = 1,
+                Symbol = "REC",
+                Ilosc = "1",
+                Towar = "PPPZZZ"
+            };
+
+            var SkladnikRecepturyInfo = new XLSkladnikRecepturyInfo_20201
+            {
+                Wersja = 20201,
+                Ilosc = "1",
+                Towar = "SAP001"
+            };
+
+    
+            var ZamkniecieRecepturyInfo = new XLZamkniecieRecepturyInfo_20201
+            {
+                Wersja = 20201,
+             };
+
+
+            var res = cdn_api.cdn_api.XLNowaReceptura(SessionID,ref IDReceptury, RecepturaInfo);
+            var res1 = cdn_api.cdn_api.XLDodajSkladnikReceptury(ref IDReceptury, SkladnikRecepturyInfo);
+            var res2 = cdn_api.cdn_api.XLZamknijRecepture(IDReceptury, ZamkniecieRecepturyInfo);
+
+
+
+        }
+    }
+}
